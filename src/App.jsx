c77d6c9b1c5e0411e5ed5e1aaf82a1152a02d1c7
Remy-Post <166053369+Remy-Post@users.noreply.github.com>
2025-12-27 13:05:58 -0500

    integrate project display with mock data, add Project component, and enhance portfolio layout using Tailwind classes

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,82 @@
-
+import { useEffect, useState } from "react";
 import Footer from "./Footer.jsx";
+import Project from "./Project.jsx";
 
 export default function App() {
+    const [isLoading, setLoading] = useState(true);
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        fetch("mockdata.json").then(res => res.json()).then(data => {
+            setProjects(data.projects);
+            setLoading(false);
+            console.log(data);
+        });
+    }, []);
+
     return(
         <>
             <div className="flex">
-                <section className="static h-[90vh] w-[50%] bg-gray-400">
-                    <div>
+                <section className="static h-screen w-[50%] bg-gray-400 flex flex-col justify-evenly py-10">
+                    <div className="self-center">
                         <h3>Hi,</h3>
                         <h1>I'm Remy</h1>
                     </div> {/*Name*/}
 
-                    <div> {/*Table of Contents*/}
+                    <div className="ml-20"> {/*Table of Contents*/}
                         <p>About Me</p>
                         <p>Projects</p>
                         <p>Contacts</p>
                     </div>
 
-                    <article> {/*Icons*/}
-                        <div></div>
+                    <article className="flex gap-5 ml-20"> {/*Icons*/}
+                        <a href="Resume.pdf" target="_blank" >
+                            <img src="Document.svg" alt="Document Icon" className="w-10 h-10" />
+                        </a>
+                        <a href="https://github.com/Remy-Post/" target="_blank">
+                            <img src="Github.svg" alt="Github Icon" className="w-10 h-10"/>
+                        </a>
+                        <a href="tel:+19053926023" target="_blank">
+                            <img src="Phone.svg" alt="Phone Icon" className="w-10 h-10" />
+                        </a>
+                        <a href="mailto:remy.post.06@gmail.com" target="_blank">
+                            <img src="Gmail.svg" alt="Gmail Icon" className="w-10 h-10" />
+                        </a>
                     </article>
                 </section>
-                <section className="static h-[90vh] w-[50%] bg-lime-200">
-                    <div></div> {/*About Me*/}
-                    <div></div> {/*Projects*/}
+                <section className="static h-screen w-[50%] bg-lime-200 py-10">
+                    <div className="w-[90%] mx-auto">
+                        <h3 className="font-bold text-2xl text-center">About Me</h3>
+                        <TextHolder>
+                            <span className="text-xl">I</span> am currently a second-year student concurrently pursuing an Honours Bachelor of Science in Computer Science at Lakehead University and a Computer Programmer diploma at Georgian College.
+                        </TextHolder>
+                        <TextHolder>
+                            <span className="text-xl">M</span>y technical expertise lies in building scalable, user-friendly full-stack applications. I specialize in the React ecosystem (including Next.js and Redux), Python automation, and ASP.NET Core.
+                        </TextHolder>
+                        <TextHolder>
+                            <span className="text-xl">B</span>eyond the screen, I value leadership and community building. I've honed my communication and organizational skills serving as VP of Communications for the Georgian College Residence Student Council and as a Camp Counsellor.
+                        </TextHolder>
+                        <TextHolder>
+                            <span className="text-xl">W</span>hen I'm not coding, you can find me analyzing movies, strategizing over board games, or relaxing with video games.
+                        </TextHolder>
+                    </div> {/*About Me*/}
+                    <div className="mt-10">
+                        <h3 className="text-center font-bold text-2xl">Projects</h3>
+                        {
+                            isLoading == true ? <div>Loading...</div> : projects.map( ((project, index) => (
+                                <Project key={index} {...project} />
+                            )))
+                        }
+                    </div> {/*Projects*/}
                     <div></div> {/*Contacts*/}
                 </section>
             </div>
         </>
+    )
+}
+
+function TextHolder({children}){
+    return(
+        <p className="my-3 rounded">{children}</p>
     )
 }
---
+++ src/Project.jsx
@@ -0,0 +1,95 @@
+//Helper used to return a 'project element'
+export default function Project({...params}){
+    const colors = {
+        gray: "text-gray-700",
+        yellow: "text-amber-300",
+        blue: "text-cyan-400",
+        red: "text-pink-500",
+        orange: "text-orange-400",
+        main: "text-black",
+        green: "text-green-500"
+
+    }
+    return(
+        <div className="">
+            {/*Header*/}
+            <div className="grid grid-cols-3">
+                <div className="w-20 flex justify-evenly">
+                    <div className="bg-cyan-400 w-[3vh] h-[3vh] rounded-4xl"></div>
+                    <div className="bg-pink-500 w-[3vh] h-[3vh] rounded-4xl"></div>
+                    <div className="bg-amber-300 w-[3vh] h-[3vh] rounded-4xl"></div>
+                </div>
+                <h3 className="text-2xl font-bold text-center self-center">{params.name}</h3>
+                {/*Icon*/}
+                <div className="flex justify-end">
+                    <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 20 20"
+                         aria-hidden="true" className="h-7 w-7 self-end hover:cursor" height="1em" width="1em"
+                         xmlns="http://www.w3.org/2000/svg">
+                        <path fillRule="evenodd"
+                              d="M4.25 5.5a.75.75 0 00-.75.75v8.5c0 .414.336.75.75.75h8.5a.75.75 0 00.75-.75v-4a.75.75 0 011.5 0v4A2.25 2.25 0 0112.75 17h-8.5A2.25 2.25 0 012 14.75v-8.5A2.25 2.25 0 014.25 4h5a.75.75 0 010 1.5h-5z"
+                              clipRule="evenodd"></path>
+                        <path fillRule="evenodd"
+                              d="M6.194 12.753a.75.75 0 001.06.053L16.5 4.44v2.81a.75.75 0 001.5 0v-4.5a.75.75 0 00-.75-.75h-4.5a.75.75 0 000 1.5h2.553l-9.056 8.194a.75.75 0 00-.053 1.06z"
+                              clipRule="evenodd"></path>
+                    </svg>
+                </div>
+            </div>
+            {/*Body*/}
+            <hr className="text-blue-100 bg-black rounded-4xl h-[2px] px-3 my-2" />
+            <div>
+                <div> {/*Starting*/}
+                    <span className={colors.red}>const</span>
+                    <span className={colors.main}> project </span>
+                    <span className={colors.red}>=</span>
+                    <span className={colors.gray}>{' {'}</span>
+                </div>
+                <div className="mx-3.5"> {/* Name*/}
+                    <span className={colors.main}>type:</span>
+                    <span className={colors.gray}> '</span>
+                    <span className={colors.blue}>{params.type}</span>
+                    <span className={colors.yellow}>{params.name}</span>
+                    <span className={colors.gray}>'</span>
+                </div>
+                <div className="mx-3.5"> {/* Tech Stack */}
+                    <span className={colors.main}>Tools: </span>
+                    <span className={colors.gray}>[</span>
+                    <span className={colors.gray}>'</span>
+                    {
+                        params.techStack.map((t, index) => (
+                            <span key={index}>
+                                <span key={index} className={colors.main}>{t}</span>
+                                <span className={colors.gray}>'</span>
+                            </span>
+                        ))
+                    }
+                    <span className={colors.gray}>]</span>
+                </div>
+                <div className="mx-3.5"> {/*Role*/}
+                    <span className={colors.main}>myRole: </span>
+                    <span className={colors.orange}>{params.role}</span>
+                </div>
+                <div className="mx-3.5"> {/*Description*/}
+                    <span className={colors.main}>Description: </span>
+                    <span className={colors.blue}>{params.description}</span>
+                    <span className={colors.gray}>,</span>
+                </div>
+                {params.liveLink && <div className="mx-3.5">
+                    <span className={colors.main}>Live link: </span>
+                    <span className="text-blue-600 underline underline-offset-2">{params.link}</span>
+                </div>}
+                {params.githubLink && <div className="mx-3.5">
+                    <span className={colors.main}>Source: </span>
+                    <span className="text-blue-600">
+                        {params.githubLink}</span>
+                </div>}
+                <div> {/*Ending*/}
+                    <span className={colors.gray}>{"}"};</span>
+                </div>
+            </div>
+            {/*Footer*/}
+            <div>
+
+            </div>
+        </div>
+    )
+}
