73b9fa56065ceee9087a077df18c2f1300fbcf2d
Remy-Post <166053369+Remy-Post@users.noreply.github.com>
2025-12-30 22:51:34 -0500

    integrate GitHub activity calendar using react-github-calendar; update navigation icons and refactor GitHub page layout

--- src/GitHub.jsx
+++ src/GitHub.jsx
@@ -1,16 +1,29 @@
-﻿import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
-import GitHubCalendar from "react-github-calendar";
+import { useInView } from "react-intersection-observer";
+
+import Project from "./Project.jsx";
+
+
+import  {GitHubCalendar} from "react-github-calendar";
 
 export default function GitHub() {
+    const [isLoading, setLoading] = useState(true);
     const [keyWords, setKeyWords] = useState([]);
     const [activeKeywordIndex, setActiveKeywordIndex] = useState(0);
+    const [projects, setProjects] = useState([]);
+
+    const { ref : githubRef, inView : isGithubInView } = useInView();
+    const { ref : projectsRef, inView : isProjectsInView } = useInView();
 
     useEffect(() => {
         fetch("mockdata.json").then(res => res.json()).then(data => {
             setKeyWords(data.keyWords);
-        });
+            setProjects(data.projects);
+
+            setLoading(false);
+        })
+            .catch(error => console.error("Error fetching data:", error));
     }, []);
 
     useEffect(() => {
@@ -40,9 +53,8 @@ export default function GitHub() {
 
                 <div className="ml-20">
                     <nav className="flex flex-col border-l-2">
-                        <NavItem href="/">About Me</NavItem>
+                        <a className="relative w-max pl-8 p-3 py-2.5 text-xl font-medium transition-all duration-300 block -ml-[2px] outline-none border-transparent text-[#586574] hover:text-[#0B1724]" href="/" target="_blank">GitHub Calendar</a>
                         <NavItem href="/">Projects</NavItem>
-                        <NavItem href="/">Work Experience</NavItem>
                     </nav>
                 </div>
 
@@ -59,19 +71,34 @@ export default function GitHub() {
             {/*-----------------------------------------*/} {/*Right Side*/}
 
             <section className="w-[99%] bg-[#FFFFFF] py-10">
-                <div className="flex justify-evenly w-full mx-auto p-4 my-6"> {/*Title*/}
-                    <h3 className="text-3xl font-bold">GitHub</h3>
-                    <h3 className="text-2xl italic text-[#6C5F6F]">Remy-Post</h3>
-                </div>
-                <GitHubCalendar
-                    username="Remy-Post"
-                    theme="dark"
-
-                />
+                <div ref={githubRef}>
+                    <div className="flex justify-evenly w-full mx-auto p-4 my-6"> {/*Title*/}
+                        <h3 className="text-3xl font-bold">GitHub</h3>
+                        <h3 className="text-2xl italic text-[#6C5F6F]">Remy-Post</h3>
+                    </div> {/*Title*/}
+                        <div className="w-[95%] mx-auto">
+                        <GitHubCalendar
+                            username="Remy-Post"
+                            className="w-full h-full"
+                        />
+                    </div> {/*Github Calendar*/}
+                    </div>
+                <div ref={projectsRef}>
+                    <h3 className="text-4xl font-bold mt-20 ml-13">Projects</h3>
+                    {
+                        isLoading == true ? <div>Loading...</div> : projects.map( ((project, index) => (
+                            <div key={index} className="p-4 m-6 bg-[#FFFFFF] rounded-4xl project">
+                                <Project {...project} />
+                            </div>
+                        )))
+                    }
+                </div> {/*Projects*/}
             </section>
         </div>
     )
 }
+
+import { Link } from "react-router-dom";
 
 function NavItem({href, children}) {
     return (
